00f02664d4d2654bf764ac46ace231f9f5bf6a99
deepakmench18 <deepakmench18@gmail.com>
2021-06-02 23:30:04 +0530

    Read the value from dictionary into the array

--- sorting_arithmetic.sh
+++ sorting_arithmetic.sh
@@ -1,6 +1,16 @@
+echo "Enter Number a, b and c"
+read a b c
+cnt=0
 declare -A res
 res['a+b*c']=$(($a+$b*$c))
 res['c+a/b']=$(($c+$a/$b))
 res['a*b+c']=$(($a*$b+$c))
 res['a%b+c']=$(($a%$b+$c))
 echo "Results of computation =${res[*]}"
+names=("${!res[@]}")
+for arr_ref in "${names[@]}"; do
+    arr[cnt]=${res[$arr_ref]}
+    ((cnt++))
+done
+
+echo "values in array arr = ${arr[@]}"
